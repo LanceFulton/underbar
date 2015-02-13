b29a232b4abc24b4c64e03440bfabce2961a9e84
LanceFulton <lsfaccounts@gmail.com>
2015-02-13 17:51:05 -0600

    Filter passes.

--- src/underbar.js
+++ src/underbar.js
@@ -24,7 +24,7 @@
    *
    * The .first function is implemented for you, to help guide you toward success
    * in your work on the following functions. Whenever you see a portion of the
-   * assignment pre-completed, be sure to read and understanding it fully before
+   * assignment pre-completed, be sure to read and understand it fully before
    * you proceed. Skipping this step will lead to considerably more difficulty
    * implementing the sections you are responsible for.
    */
@@ -32,7 +32,7 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
-    return n === undefined ? array[0] : array.slice(0, n);
+    return (n === undefined ? array[0] : array.slice(0, n));
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
@@ -81,6 +81,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    for (var i=0 ; i<collection.length ; i++){
+      if (test(collection[i])) {
+        result.push(collection[i]);
+      }
+    }
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
