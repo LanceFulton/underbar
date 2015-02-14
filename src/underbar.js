6114612ac2b93949b2842f067623442f8ef6b729
LanceFulton <lsfaccounts@gmail.com>
2015-02-14 14:20:09 -0600

    Unique passes.

--- src/underbar.js
+++ src/underbar.js
@@ -90,36 +90,6 @@
     return result;
   };
 
-/*
-  // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
-    var result = collection;
-    var antiResult = _.filter(collection, test);
-    for (var i=0 ; i<antiResult.length ; i++){
-      var badElement = result.indexOf(antiResult[i]);
-      result.splice(badElement, 1);
-    }
-    return result;
-  };
-*/
-/*
-  // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, test) {
-    var result = [];
-    var antiResult = _.filter(collection, test);
-    for (var i=0 ; i<collection.length ; i++){
-      for (var j=0 ; j<antiResult.length ; j++){
-        if (collection[i] !== antiResult[j]){
-          result.push(collection[i]);
-        }
-      }
-    }
-    return result;
-  };
-*/
-
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     var result = [];
@@ -133,6 +103,14 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    for (var i=0 ; i<array.length; i++) {
+      var value = array[i];
+      if (_.indexOf(result, value) < 0) {
+        result.push(value);
+      }
+    }
+    return result;
   };
 
 
